5f22310c553c58fc895d45cc5a551d26caa7fc66
陳衍良 <alec@cpan.org>
2008-09-06 07:25:26 +0000

    Import of ALEC/Text-ASCIITable-TW-v0.03 from CPAN.
    gitpan-cpan-distribution: Text-ASCIITable-TW
    gitpan-cpan-version:      v0.03
    gitpan-cpan-path:         ALEC/Text-ASCIITable-TW-v0.03.tar.gz
    gitpan-cpan-author:       ALEC
    gitpan-cpan-maturity:     released

--- lib/Text/ASCIITable/TW.pm
+++ lib/Text/ASCIITable/TW.pm
@@ -9,11 +9,11 @@ Text::ASCIITable::TW - add TW support for Text::ASCIITable
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-use version; our $VERSION = qv('0.02');
+use version; our $VERSION = qv('0.03');
 
 
 =head1 SYNOPSIS
